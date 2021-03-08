43e62141ffaf350f85ff6751d8b9bc99213e5d58
artsasse <artsasse@gmail.com>
2021-03-08 20:23:12 -0300

    + planets and textures

--- threejs/main.js
+++ threejs/main.js
@@ -29,13 +29,8 @@ function init() {
     camera.position.y = 20;
     camera.lookAt( 0, 0, -4);
     
-    
-
     // Setting up scene
     scene = new THREE.Scene();
-    // Earth
-    earth = createSphere(1, 20, 'texture/earth.jpg', 'Phong');
-    earth.position.z = -12;
 
     // Sun (Sphere + Light)
     sun = createSphere(1.25, 20, 'texture/sun.jpg');
@@ -44,13 +39,59 @@ function init() {
     sunlight...;
     sun...
     */
-
-    /* Complete: add 
-    other planets */ 
-
-    scene.add(earth);
     scene.add(sun);
 
+    /* Complete: add 
+    other planets */
+    // Mercury
+    mercury = createSphere(1, 20, 'texture/mercury.jpg', 'Phong');
+    mercury.position.z = -6;
+    sun.add(mercury);
+
+    // Venus
+    venus = createSphere(1, 20, 'texture/venus_atmosphere.jpg', 'Phong');
+    venus.position.z = -9;
+    sun.add(venus);
+
+    // Earth
+    earth = createSphere(1, 20, 'texture/earth.jpg', 'Phong');
+    earth.position.z = -12;
+    sun.add(earth);
+
+    // Moon
+    moon = createSphere(0.5, 20, 'texture/moon.jpg', 'Phong');
+    moon.position.z = -13;
+    earth.add(moon);
+
+    // Mars
+    mars = createSphere(1, 20, 'texture/mars.jpg', 'Phong');
+    mars.position.z = -15;
+    sun.add(mars);
+
+    // Jupiter
+    jupiter = createSphere(1, 20, 'texture/jupiter.jpg', 'Phong');
+    jupiter.position.z = -18;
+    sun.add(jupiter);
+
+    // Saturn
+    saturn = createSphere(1, 20, 'texture/saturn.jpg', 'Phong');
+    saturn.position.z = -21;
+    sun.add(saturn);
+
+    // Saturn Ring
+    saturn_ring = createRing(1.2, 1.6, 8, 'texture/saturn_ring_alpha.png', 'Phong');
+    saturn_ring.position.z = -21;
+    saturn.add(saturn_ring);
+
+    // Uranus
+    uranus = createSphere(1, 20, 'texture/uranus.jpg', 'Phong');
+    uranus.position.z = -24;
+    sun.add(uranus);
+
+    // Neptune
+    neptune = createSphere(1, 20, 'texture/neptune.jpg', 'Phong');
+    neptune.position.z = -27;
+    sun.add(neptune);
     
     // Adding both renderer and stats to the Web page, also adjusting OrbitControls
     stats = new Stats();
