e918553b2b163709a4071ee2215f5b0df1a4deb5
artsasse <artsasse@gmail.com>
2021-03-09 00:21:55 -0300

    + rotations and translations

--- threejs/main.js
+++ threejs/main.js
@@ -15,6 +15,37 @@ var sun, earth;
 // Light in the scene 
 var sunlight;
 
+// Angle increments per frame representing planets' rotations
+const EARTH_DAY = 1.2;
+const MERCURY_DAY = EARTH_DAY / 58.6;
+const VENUS_DAY = EARTH_DAY / 243;
+const MARS_DAY = EARTH_DAY / 1.03;
+const JUPITER_DAY = EARTH_DAY / 0.41;
+const SATURN_DAY = EARTH_DAY / 0.45;
+const URANUS_DAY = EARTH_DAY / 0.72;
+const NEPTUNE_DAY = EARTH_DAY / 0.67;
+
+
+// Angle increments per frame representing planets' revolutions
+const EARTH_YEAR = EARTH_DAY / 365;
+const MERCURY_YEAR = EARTH_DAY / 87.97;
+const VENUS_YEAR = EARTH_DAY / 224.7;
+const MARS_YEAR = EARTH_YEAR / 1.88;
+const JUPITER_YEAR = EARTH_YEAR / 11.86;
+const SATURN_YEAR = EARTH_DAY / 29.46;
+const URANUS_YEAR = EARTH_DAY / 84;
+const NEPTUNE_YEAR = EARTH_DAY / 164.79;
+
+
+// Angle increments per frame for Moon
+const MOON_ROTATION = EARTH_DAY / 30;
+const MOON_REVOLUTION = EARTH_DAY / 30;
+
+// Solar System Center
+const SUN_CENTER = new THREE.Vector3(0, 0, -3);
+
+// Earth Center
+const EARTH_CENTER = new THREE.Vector3(0, 0, -12);
 
 function init() {
 
@@ -34,11 +65,10 @@ function init() {
 
     // Sun (Sphere + Light)
     sun = createSphere(1.25, 20, 'texture/sun.jpg');
-    sun.position.z = -3;
-    /* Complete: add light
-    sunlight...;
-    sun...
-    */
+    // sun.position.z = -3;
+    sun.position = SUN_CENTER;
+    sunlight = new THREE.PointLight( 0xffffffff, 1.5, 0, 2);
+    sun.add(sunlight);
     scene.add(sun);
 
     /* Complete: add 
@@ -59,8 +89,8 @@ function init() {
     sun.add(earth);
 
     // Moon
-    moon = createSphere(0.5, 20, 'texture/moon.jpg', 'Phong');
-    moon.position.z = -13;
+    moon = createSphere(0.2, 20, 'texture/moon.jpg', 'Phong');
+    moon.position.z = -2;
     earth.add(moon);
 
     // Mars
@@ -79,8 +109,8 @@ function init() {
     sun.add(saturn);
 
     // Saturn Ring
-    saturn_ring = createRing(1.2, 1.6, 8, 'texture/saturn_ring_alpha.png', 'Phong');
-    saturn_ring.position.z = -21;
+    saturn_ring = createRing(1.2, 1.6, 30, 'texture/saturn_ring_alpha.png', 'Phong');
+    // saturn_ring.rotation.x = 10;
     saturn.add(saturn_ring);
 
     // Uranus
@@ -133,8 +163,29 @@ function animate() {
 
     stats.update();
     renderer.render( scene, camera );
-    earth.rotation.y+=0.02
-    
+
+    // Rotations
+    mercury.rotation.y+=MERCURY_DAY;
+    venus.rotation.y+=VENUS_DAY;
+    earth.rotation.y+=EARTH_DAY;
+    mars.rotation.y+=MARS_DAY;
+    jupiter.rotation.y+=JUPITER_DAY;
+    saturn.rotation.y+=SATURN_DAY;
+    uranus.rotation.y+=URANUS_DAY;
+    neptune.rotation.y+=NEPTUNE_DAY;
+    moon.rotation.y+=MOON_ROTATION;
+
+    // Revolutions
+    axisY = new THREE.Vector3(0, 1, 0);
+    mercury.rotateAroundPoint(SUN_CENTER, MERCURY_YEAR, axisY, true);
+    venus.rotateAroundPoint(SUN_CENTER, VENUS_YEAR, axisY, true);
+    earth.rotateAroundPoint(SUN_CENTER, EARTH_YEAR, axisY, true);
+    mars.rotateAroundPoint(SUN_CENTER, MARS_YEAR, axisY, true);
+    jupiter.rotateAroundPoint(SUN_CENTER, JUPITER_YEAR, axisY, true);
+    saturn.rotateAroundPoint(SUN_CENTER, SATURN_YEAR, axisY, true);
+    uranus.rotateAroundPoint(SUN_CENTER, URANUS_YEAR, axisY, true);
+    neptune.rotateAroundPoint(SUN_CENTER, NEPTUNE_YEAR, axisY, true);
+    moon.rotateAroundPoint(EARTH_CENTER, MOON_REVOLUTION, axisY, true);
 
 }
 
